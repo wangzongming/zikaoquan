1b74762a67eb852719f912e03ef016d38000ba60
xm <1746809408@qq.com>
2020-09-01 00:31:48 +0000

    设置跨域

--- api/addOnline.js
+++ api/addOnline.js
@@ -3,6 +3,11 @@ const dataUnit = require('./dataUnit');
 
 module.exports = (req, res) => {
     // const { name = 'World' } = req.query
+    
+    // 设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Origin", "*"); 
+　　//跨域允许的请求方式
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,PATCH");
     try {
         const onlineNumber = dataUnit.onlineNumber.get();
         dataUnit.onlineNumber.set((onlineNumber + 1));
--- api/getOnline.js
+++ api/getOnline.js
@@ -1,6 +1,10 @@
 const dataUnit = require('./dataUnit');
 
 module.exports = (req, res) => { 
+    // 设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Origin", "*"); 
+　　//跨域允许的请求方式
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,PATCH");
     try {
         const onlineNumber = dataUnit.onlineNumber.get(); 
         res.status(200).send(onlineNumber);
