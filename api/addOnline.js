0791202bc33324d8271605fa10ff4649da8e62da
xm <1746809408@qq.com>
2020-09-01 00:40:59 +0000

    update

--- api/addOnline.js
+++ api/addOnline.js
@@ -3,15 +3,20 @@ const dataUnit = require('./dataUnit');
 
 module.exports = (req, res) => {
     // const { name = 'World' } = req.query
-    
+
     // 设置允许跨域的域名，*代表允许任意域名跨域
-      res.setHeader('access-control-allow-origin', '*');
+    res.setHeader('access-control-allow-origin', '*');
     try {
         const onlineNumber = dataUnit.onlineNumber.get();
         dataUnit.onlineNumber.set((onlineNumber + 1));
-        res.status(200).send(true);
+        res.status(200).send(JSON.stringify({
+            success: true, 
+        }));
     } catch (err) {
-        res.status(200).send(false);
+        res.status(200).send(JSON.stringify({
+            success: false,
+            err: err
+        }));
     }
 
 }
--- api/getOnline.js
+++ api/getOnline.js
@@ -1,10 +1,10 @@
 const dataUnit = require('./dataUnit');
 
-module.exports = (req, res) => { 
+module.exports = (req, res) => {
     // 设置允许跨域的域名，*代表允许任意域名跨域
-      res.setHeader('access-control-allow-origin', '*');
+    res.setHeader('access-control-allow-origin', '*');
     try {
-        const onlineNumber = dataUnit.onlineNumber.get(); 
+        const onlineNumber = dataUnit.onlineNumber.get();
         res.status(200).send(onlineNumber);
     } catch (err) {
         res.status(200).send(false);
