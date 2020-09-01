9c32f8b530160d04999a6ae23994f25776955a2d
xm <1746809408@qq.com>
2020-09-01 08:07:52 +0800

    Create addOnline.js

---
+++ api/addOnline.js
@@ -0,0 +1,9 @@
+module.exports = (req, res) => {
+  // const { name = 'World' } = req.query
+   try{ 
+    res.status(200).send(`false`);
+   }catch(err){ 
+    res.status(200).send(`false`);
+   }
+  
+}
