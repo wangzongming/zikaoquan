7acc78d977e91016c2fe9788dbbc247511f79b16
xm <1746809408@qq.com>
2020-09-02 13:40:45 +0000

    update

--- src/App.js
+++ src/App.js
@@ -13,12 +13,11 @@ function App() {
     getDate();
   }, []);
   return (
-    <main>
-
+    <main> 
         <h2>一个专注于学习的计时器</h2>
-        <div>下载地址：</div>
+        <div style={{color:"#ccc"}}>下载地址</div>
         <div>安卓v1.0：<a href={require('./app.apk')}>点击下载</a></div>
-        <div>安卓：<a>开发中...</a></div>
+        <div>安卓v1.0：<a>开发中...</a></div>
     </main>
   );
 }
