6a9d2ff95da60569e7456c80cb8a88b48dfc5276
xm <1746809408@qq.com>
2020-09-03 07:30:28 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
     <main> 
         <h2>一个专注于学习的计时器</h2>
         <div style={{color:"#ccc"}}>下载地址</div>
-        <div>安卓v1.0：<a href={"https://menhechuanghb.com/zikaoquanv1.0.apk"}>点击下载</a></div>
-        <div>安卓v1.0：<a>开发中...</a></div>
+        <p>安卓v1.0：<a href={"https://menhechuanghb.com/zikaoquanv1.0.apk"}>点击下载</a></p>
+        <p>苹果v1.0：<a>开发中...</a></p>
     </main>
   );
 }
